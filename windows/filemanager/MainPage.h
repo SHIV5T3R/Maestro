172e03ee2bbc037227e0b5857a92c51f2d8a8ecf
SHIV5T3R <shiv@shivster.tech>
2023-06-16 22:15:26 +0100

    Task: Project Setup

---
+++ windows/filemanager/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::filemanager::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::filemanager::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
