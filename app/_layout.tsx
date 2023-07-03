559a44391c5cec656bc9b15e1f48939989c384ae
Gabriel Rasteli <gabrielrasteli3@gmail.com>
2023-07-03 13:54:58 -0300

    feat: Add context menu when long pressing file or folder

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,14 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
+import { MenuProvider } from 'react-native-popup-menu';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -40,13 +45,13 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <>
+    <MenuProvider>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
         </Stack>
       </ThemeProvider>
-    </>
+    </MenuProvider>
   );
 }
